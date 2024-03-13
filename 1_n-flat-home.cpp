52a51c0d389b2b7ac19153d31890c33f4638a54e
onsntt <onsnt@mail.ru>
2024-03-13 22:52:09 +0500

    N-flat_house is ready

--- 1_n-flat-home.cpp
+++ 1_n-flat-home.cpp
@@ -6,37 +6,27 @@ for (int i = 0; i< 10; i++) {
     std::cin >>people[i];
 }
 
-//for (int i =0; i < 10; i++ ){
-  //  std::cout << people[i] << "\n";
-//}
-//std::cout <<"Enter three flat numbers\n";
-//int numbers[3];
-//for (int i = 0; i <3; i++) {
-//std::cin >> numbers[i];
-//}
-//std::cout << "Ladies ang gentlemen, here is the list of apartment dwellers in source flats: \n";
-//for (int i = 0; i < 3; i++){
-//    std::cout << "Flat " << numbers[i] << " - " << people[numbers[i]] << "\n"; 
-//}
+
 for (int i =0; i < 3; ) {
     int flat = 0; 
-    std::cout << "Enter the flat number\n";
+    std::cout << "Enter the flat number (1 - 10)\n";
     std::cin >> flat;
-    if (flat <0 || flat >9) std::cout << "Incorrect input. You are loosing your trys! \n";
-    else std::cout << "Flat " <<  flat << " - " << people[flat] << "\n"; 
+    if (flat <1 || flat >10) std::cout << "Incorrect input. You are loosing your trys! \n";
+    else std::cout << "Flat " <<  flat << " - " << people[flat-1] << "\n"; 
     i++;
 }
 std::cout << "And now - a little joke ;-)\n";
 for (int i =0; i < 10; i++ ){
    //std::cout << people[i] << "\n";
 }
- std::cout <<"Enter three flat numbers\n";
+ std::cout <<"Enter three flat numbers! \n";
  int numbers[3];
  for (int i = 0; i <3; i++) {
  std::cin >> numbers[i];
 }
  std::cout << "Ladies ang gentlemen, here is the list of apartment dwellers in source flats: \n";
  for (int i = 0; i < 3; i++){
-    std::cout << "Flat " << numbers[i] << " - " << people[numbers[i]] << "\n";
+    if (numbers[i] < 0 || numbers[i] > 10) std::cout << "Incorrect input!\n";
+    else  std::cout << "Flat " << numbers[i] << " - " << people[numbers[i]-1] << "\n";
 }
 }
