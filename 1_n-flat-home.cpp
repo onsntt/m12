57bceca9950966f45a8e7b721471e444ff133085
onsntt <onsnt@mail.ru>
2024-03-13 22:12:42 +0500

    ttt

--- 1_n-flat-home.cpp
+++ 1_n-flat-home.cpp
@@ -26,4 +26,17 @@ for (int i =0; i < 3; ) {
     else std::cout << "Flat " <<  flat << " - " << people[flat] << "\n"; 
     i++;
 }
+std::cout << "And now - a little joke ;-)\n";
+for (int i =0; i < 10; i++ ){
+   //std::cout << people[i] << "\n";
+}
+ std::cout <<"Enter three flat numbers\n";
+ int numbers[3];
+ for (int i = 0; i <3; i++) {
+ std::cin >> numbers[i];
+}
+ std::cout << "Ladies ang gentlemen, here is the list of apartment dwellers in source flats: \n";
+ for (int i = 0; i < 3; i++){
+    std::cout << "Flat " << numbers[i] << " - " << people[numbers[i]] << "\n";
+}
 }
