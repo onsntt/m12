b828b26a87829c779264212dfba1945ab97d6166
onsntt <onsnt@mail.ru>
2024-03-16 16:01:37 +0500

    Task2 (Quick Sort) added

--- 2_qsort.cpp
+++ 2_qsort.cpp
@@ -1,55 +1,59 @@
 #include <iostream>
 
+// Zadacha 2
+// this is chisto normalnaya solyanka 
+//first of all - get pivot position, after that - swaps
 
-int partition(int arr[], int start, int end)
+
+int partition(float arr[], int start, int end)
 {
 
-    int pivot = arr[start];
+    float pivot = arr[start];
 
     int count = 0;
-    for (int i = start + 1; i <= end; i++)
+    for (int u = start + 1; u <= end; u++)
     {
-        if (arr[i] <= pivot)
+        if (arr[u] >= pivot)
             count++;
     }
 
-    // Giving pivot element its correct position
+    // Get pivot position
     int pivotIndex = start + count;
     std::swap(arr[pivotIndex], arr[start]);
-std::cout << "ex start is pivot " << arr[pivotIndex] << "ex-pivot index is now start " << arr[start] << "\n";
-    // Sorting left and right parts of the pivot element
-    int i = start, j = end;
+    //std::cout << "ex-start is pivot now - " << arr[pivotIndex] << ", ex-pivot is now start - " << arr[start] << "\n";
+    // Sorting left and right parts 
+    int e = start, j = end;
 
-    while (i < pivotIndex && j > pivotIndex)
+    while (e < pivotIndex && j > pivotIndex)
     {
 
-        while (arr[i] <= pivot)
+        while (arr[e] >= pivot)
         {
-            i++;
+            e++;
         }
 
-        while (arr[j] > pivot)
+        while (arr[j] < pivot)
         {
             j--;
         }
 
-        if (i < pivotIndex && j > pivotIndex)
+        if (e < pivotIndex && j > pivotIndex)
         {
-            std::swap(arr[i++], arr[j--]);
+            std::swap(arr[e], arr[j]);
         }
     }
 
-    return pivotIndex;
+     return pivotIndex;
 }
 
-void quickSort(int arr[], int start, int end)
+void quickSort(float arr[], int start, int end)
 {
 
-    // base case
+    
     if (start >= end)
         return;
 
-    // partitioning the array
+    // cutting the array
     int p = partition(arr, start, end);
 
     // Sorting the left part
@@ -62,20 +66,26 @@ void quickSort(int arr[], int start, int end)
 int main()
 {
 
-    int arr[] = {9, 3, 4, 2, 1};
-    int n = 5;
-    for (int i = 0; i<5; i++)
-    {
-      std::cout << arr[i] << ", ";
+    std::cout << "Enter 15 float numbers. I trust you and don't check your input.\n";
 
+    float arr[15];
+    int n = 15;
+    for (int i = 0; i <15; i++)
+    {
+    std::cin >> arr[i];
+
+    }
+    for (int i = 0; i < 15; i++)
+    {
+        std::cout << arr[i] << "\t ";
     }
     std::cout << "\n";
 
-        quickSort(arr, 0, n - 1);
+    quickSort(arr, 0, n - 1);
 
     for (int i = 0; i < n; i++)
     {
-        std::cout << arr[i] << " ";
+        std::cout << arr[i] << "\t ";
     }
 
     return 0;
